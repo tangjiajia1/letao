bff2d13342547f2a401005b29dd9ad1808c547d8
jia <2862151256@qq.com>
2018-11-27 20:45:59 +0800

    进度条功能完成

--- public/1-back/js/commit.js
+++ public/1-back/js/commit.js
@@ -1,9 +1,29 @@
-/**
- * 进度条
- * 发起ajax请求时进度条开始
- * ajax请求完进度条结束
- */
 
+
+/**
+ * 进度条的使用
+ * NProgress.start();发起ajax请求时进度条 开始
+ * NProgress.done(); ajax请求完进度条     结束
+ * 
+ * ajax 全局事件
+ * .ajaxCommplete() 每个ajax完成时调用（不管是否成）
+ * .ajaxSuccess()   每个ajax只要成功了就会调用
+ * .ajaxError()     每个ajax只要失败了就会调用
+ * .ajaxSend()      每个ajax 发送前调用
+ * 
+ * .ajaxStart()     第一个ajax 请求开始时调用
+ * .ajaxStop()      所有的ajax 请求完成时调用 
+ */
+//引入了nprogress.js文件后，就有了一个全局对象NProgress对象
+  $(document).ajaxStart(function(){
+    NProgress.start();
+  })
+  $(document).ajaxStop(function(){
+    setTimeout(function(){//模拟网络延迟时间
+      NProgress.done();
+    },1000);
+ 
+  });
 
 
  /**
